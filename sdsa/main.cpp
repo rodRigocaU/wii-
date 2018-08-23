ce5199abb9ce6f2422948d4bf97feb650cd6e26f
ALUMNO-N415 <ALUMNO-N415@UCSP.AULAB>
2018-08-23 09:50:15 -0500

    Veriso 3.0

--- sdsa/main.cpp
+++ sdsa/main.cpp
@@ -4,11 +4,31 @@ using namespace std;
 
 int main()
 {
-    int a = 6;
-    a++;
-    ++a;
-    cout << a << endl;
-    cout << "C++ SUPER LENGUAJE CARRY  GG" << '/n';
-    cout << "Hello world, see you soon" << '/n';
-     return 0;
+    /*int a,b,c,d,e;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cout << "El promedio es : " << (a + b + c + d + e)/ 5 << endl;
+
+    int arr[5] = {a,b,c,d,e};
+    int n = 0;
+    int cont = 0;
+    while(n < 5)
+    {
+        cont = cont + arr[n];
+        n += 1;
+    }
+    cout << "El resultado es" << cont / 5 << endl;*/
+    short int o = 32766;
+    short int p = o + 1;
+    cout << p << endl;
+    p = p + 1;
+    cout << p  << endl;
+    int r = 2147483647;
+    cout << r + 1 << endl;
+    int v = 10;
+    cout << v << '/n' ;
+    return 0;
 }
